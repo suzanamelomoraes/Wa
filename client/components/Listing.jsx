53b485ad1b46948f8cfc96efc1d189a9d518d6f2
Andria-Hibe <andriacohibe@gmail.com>
2019-12-05 11:41:52 +1300

    Add more hard data to test rendering

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -25,144 +25,104 @@ const mahiDetails = [{
     time: 6
   },
   {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    id: 3,
+    name: 'Wanna learn Japanese',
+    category: 'Language',
+    description: 'Even just basic phrases is okay!',
     assigner: 'Andria',
     assignee: null,
     status: 'open',
     time: 3
   },
   {
-    id: 2,
-    name: 'Teach me to swim',
+    id: 4,
+    name: 'Teach me basketball',
     category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
+    description: 'Wanna be the next Kobe beef',
     assigner: 'Andrea',
     assignee: null,
     status: 'open',
     time: 6
   },
   {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    id: 5,
+    name: 'Basic JS help',
+    category: 'Technical',
+    description: 'Need help with homework!',
     assigner: 'Andria',
     assignee: null,
     status: 'open',
     time: 3
   },
   {
-    id: 2,
-    name: 'Teach me to swim',
+    id: 6,
+    name: 'Teach me volleyball',
     category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
+    description: 'Need it to impress boyz',
     assigner: 'Andrea',
     assignee: null,
     status: 'open',
     time: 6
   },
   {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    id: 7,
+    name: 'Fix my computer',
+    category: 'Computer',
+    description: 'Laptop MacBook Air 2013 would not turn on',
     assigner: 'Andria',
     assignee: null,
     status: 'open',
     time: 3
   },
   {
-    id: 2,
-    name: 'Teach me to swim',
+    id: 8,
+    name: 'Teach me billiards',
     category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
+    description: 'Wanna hit balls',
     assigner: 'Andrea',
     assignee: null,
     status: 'open',
-    time: 6
+    time: 4
   },
   {
-    id: 1,
-    name: 'Fix my headlight',
+    id: 9,
+    name: 'Install dashcam',
     category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    description: 'Already dashcam, just need installation',
+    assigner: 'Andria',
+    assignee: null,
+    status: 'open',
+    time: 2
+  },
+  {
+    id: 10,
+    name: 'Clean car',
+    category: 'Automotive',
+    description: 'Muddy as hell, but only a sedan',
+    assigner: 'Andria',
+    assignee: null,
+    status: 'open',
+    time: 1
+  },
+  {
+    id: 11,
+    name: 'Teach me Te Reo Maori',
+    category: 'Language',
+    description: 'So I can make this app',
     assigner: 'Andria',
     assignee: null,
     status: 'open',
     time: 3
   },
   {
-    id: 2,
-    name: 'Teach me to swim',
-    category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
-    assigner: 'Andrea',
-    assignee: null,
-    status: 'open',
-    time: 6
-  },
-  {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    id: 12,
+    name: 'Help clean my yard',
+    category: 'Housework',
+    description: 'Small yard but lots of leaves',
     assigner: 'Andria',
     assignee: null,
     status: 'open',
     time: 3
-  },
-  {
-    id: 2,
-    name: 'Teach me to swim',
-    category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
-    assigner: 'Andrea',
-    assignee: null,
-    status: 'open',
-    time: 6
-  },
-  {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
-    assigner: 'Andria',
-    assignee: null,
-    status: 'open',
-    time: 3
-  },
-  {
-    id: 2,
-    name: 'Teach me to swim',
-    category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
-    assigner: 'Andrea',
-    assignee: null,
-    status: 'open',
-    time: 6
-  },
-  {
-    id: 1,
-    name: 'Fix my headlight',
-    category: 'Automotive',
-    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
-    assigner: 'Andria',
-    assignee: null,
-    status: 'open',
-    time: 3
-  },
-  {
-    id: 2,
-    name: 'Teach me to swim',
-    category: 'Sports',
-    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
-    assigner: 'Andrea',
-    assignee: null,
-    status: 'open',
-    time: 6
   }]
 
 class Listing extends Component {
@@ -174,7 +134,7 @@ class Listing extends Component {
     // const { mahiDetails } = this.props //change mahi name to whatever is being sent
 
     return (
-      <Card.Group>
+      <Card.Group centered>
         {mahiDetails.map(mahi =>
           <MahiSummary key={mahi.id} {...mahi} />)}
       </Card.Group>
