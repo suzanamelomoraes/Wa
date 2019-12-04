e6f3c73672b788749b88f25e526653e03a77463d
Jeya Raniga <jayafj@gmail.com>
2019-12-05 10:25:09 +1300

    renamed Mahi to Mahi Summary

---
+++ client/components/MahiSummary.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class MahiSummary extends Component {
+  state = { }
+  render () {
+    return (
+      <h1>Mahi Summary</h1>
+    )
+  }
+}
+
+export default MahiSummary
