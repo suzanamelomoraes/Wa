df69ba621305b402b4a4e77dd3ff64545499d656
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-12-05 10:35:12 +1300

    create migration categories

--- server/db/migrations/20191205102724_categories.js
+++ server/db/migrations/20191205102724_categories.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('categories', table => {
+    table.increments('id')
+    table.string('categories')
+  })
+}
 
-exports.down = function(knex) {
-  
-};
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('categories')
+}
