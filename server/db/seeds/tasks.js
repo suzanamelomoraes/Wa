b4edcd04607eaed0b8cea7f01b1d7e0f7c299d6b
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-12-05 11:58:29 +1300

    populate tasks seed

--- server/db/seeds/tasks.js
+++ server/db/seeds/tasks.js
@@ -1,13 +1,12 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('tasks').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+      return knex('tasks').insert([
+        { id: 1, cat_id: 2, assigner: 3, assignee: 4, name: 'walk with dogs', description: 'walk with dogs', status: 'completed', time: '1' },
+        { id: 2, cat_id: 7, assigner: 4, assignee: 1, name: 'JavaScript classes', description: 'JavaScript classes with React, Redux', status: 'completed', time: '1' },
+        { id: 3, cat_id: 12, assigner: 1, assignee: 2, name: 'surf', description: 'surf on Piha beach', status: 'completed', time: '1' }
+      ])
+    })
 };
