331ec593978d9dbfa6fd9233d87bd760e5044b2b
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-12-05 12:05:43 +1300

    added image to home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
 
 const Home = () => (
-  <div>
-    <h1>This is Home </h1>
-  </div>
+  // image
+  <>
+<Container>
+  <Header
+    as='h1'
+    inverted
+    style={{
+      width: 1024,
+      height: 320,
+      display: 'inline-block',
+      opacity: 0.5,
+      backgroundImage: `url(${'images/placeholder.jpg'})`,
+      backgroundSize: 'cover',
+      fontWeight: 'normal',
+      marginBottom: 0
+    }}
+  >
+       Yooza
+  </Header>
+</Container>
+  </>
 )
 
 export default Home
+
+{ /* <div>
+    <h1>
+      Naku te rourou <br/>
+      nau te rourou <br/>
+      ka ora ai te iwi
+    </h1>
+  </div> */ }
