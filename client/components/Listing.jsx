3acc4b2d9402cfcf4801cc85c0259fd88d727fcb
Andria-Hibe <andriacohibe@gmail.com>
2019-12-05 11:05:21 +1300

    Start Listing component

--- client/components/Listing.jsx
+++ client/components/Listing.jsx
@@ -1,4 +1,29 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Card, Image } from 'semantic-ui-react'
+
+import MahiSummary from './MahiSummary'
+
+const mahiDetails = [{
+    id: 1,
+    name: 'Fix my headlight',
+    category: 'automotive',
+    description: 'Toyota Civic 2013, left headlight is broken. Might be electrical connection?',
+    assigner: 'Andria',
+    assignee: null,
+    status: 'open',
+    time: 3
+  },
+  {
+    id: 2,
+    name: 'Teach me to swim',
+    category: 'sports',
+    description: 'Adult, 23, cannot swim at all. Want to get to at least intermediate level',
+    assigner: 'Andrea',
+    assignee: null,
+    status: 'open',
+    time: 6
+  }]
 
 class Listing extends Component {
   state = {
@@ -6,8 +31,13 @@ class Listing extends Component {
   }
 
   render () {
+    // const { mahiDetails } = this.props //change mahi name to whatever is being sent
+
     return (
-      <h1>Listing</h1>
+      <Card.Group>
+        {mahiDetails.map(mahi =>
+          <MahiSummary key={mahi.id} {...mahi} />)}
+      </Card.Group>
     )
   }
 }
