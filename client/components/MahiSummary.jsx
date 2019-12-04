b1d650ecef83725cfe935106d7c0faca6ba2e39d
Jeya Raniga <34700984+JeyaRaniga@users.noreply.github.com>
2019-12-05 12:24:40 +1300

    removed hard coding

--- client/components/MahiSummary.jsx
+++ client/components/MahiSummary.jsx
@@ -21,12 +21,11 @@ export class MahiSummary extends Component {
             src='/images/avatar01.png'
           />
           <Card.Header as='h1'>{title}</Card.Header>
-          <Card.Meta as='h3'>Category <Icon name='columns' size='small'></Icon><br/><span>{category}category</span></Card.Meta>
+          <Card.Meta as='h3'>Category <Icon name='columns' size='small'></Icon><br/><span>{category}</span></Card.Meta>
           <Card.Meta as='h3'>Hours <Icon name='time' size='small'></Icon><br/><span>{hours} hours</span></Card.Meta>
-          <Card.Meta as='h3'>Needed by<br/><span>{assigner} name</span></Card.Meta>
+          <Card.Meta as='h3'>Needed by<br/><span>{assigner}</span></Card.Meta>
           <Card.Description>
             {description}
-             Mahi description Mahi description Mahi description Mahi description Mahi description Mahi description Mahi description Mahi description
           </Card.Description>
         </Card.Content>
         <Card.Content textAlign='center' extra>
