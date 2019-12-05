2a643be7443d7fd347fc9fe9cee02e05c6d01274
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-12-05 16:14:09 +1300

    create getTasks function

--- server/db/db.js
+++ server/db/db.js
@@ -1,2 +1,12 @@
-
 const connection = require('./connection')
+
+function getTasks (db = connection) {
+  return db('tasks')
+    .join('categories', 'tasks.cat_id', 'categories.id')
+    .join('users', 'tasks.assigner', 'users.id')
+    .select()
+}
+
+module.exports = {
+  getTasks
+}
