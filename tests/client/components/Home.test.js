d47b9d5e2dd6e0f2438a0533b368862dcca17375
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-12-05 18:10:39 +1300

    add tests to Home component

---
+++ tests/client/components/Home.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import Home from '../../../client/components/Home'
+
+test('Testing for true is truthy', () => {
+  expect(true).toBeTruthy()
+})
+
+describe('Test <Home /> component', () => {
+  it('Renders Image correctly', () => {
+    // Arrange
+
+    // Act
+
+    // Assert
+
+  })
+})
