a0d86588f6d83fa212a8804beb173a87f8bd157f
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-12-05 18:02:05 +1300

    Add responiveness to grid

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Header, Divider, Image } from 'semantic-ui-react'
+import { Container, Header, Divider, Image, Grid, Segment } from 'semantic-ui-react'
 
 const inlineText = <div>
   <Header as='h1' textAlign='center'>
@@ -19,7 +19,13 @@ const inlineText = <div>
   </Header>
 </div>
 
-const Home = () => (
+const Home = () => {
+  const centerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap' }
+  return (
   <>
   <Container
     style={{ width: '100%',
@@ -27,6 +33,7 @@ const Home = () => (
     }}
   >
     <Image
+      bordered
       style={{
         poistion: 'absolute',
         width: '100%',
@@ -46,9 +53,50 @@ const Home = () => (
       {inlineText}
     </Image>
   </Container>
+<Header style={centerStyle} size='huge'> What to do? </Header>
+
+    <Grid colums='equal' style={centerStyle}>
+
+      <Grid.Column mobile={12} tablet={8} computer={4}>
+        <Header as='h3' attached='top' >
+            This is the top
+        </Header>
+        <Segment attached>
+        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+        </Segment>
+      </Grid.Column>
+
+      <Grid.Column mobile={12} tablet={8} computer={4}>
+        <Header as='h3' attached='top' >
+This is the top
+        </Header>
+        <Segment attached>
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+        </Segment>
+      </Grid.Column>
+
+      <Grid.Column mobile={12} tablet={8} computer={4}>
+        <Header as='h3' attached='top' >
+This is the top
+        </Header>
+        <Segment attached>
+      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+        </Segment>
+      </Grid.Column>
+    </Grid>
 
   </>
 
-)
+  )
+}
 
 export default Home
