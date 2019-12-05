b04e20a547cc9fb705b93468e2800c2db91aebec
Marina Tateoka <marinaslussarek@gmail.com>
2019-12-05 23:46:26 +1300

    create test for server-side db getTasks function

---
+++ tests/server/db/tasks.test.js
@@ -0,0 +1,25 @@
+const env = require('./test.environment')
+const db = require('../../../server/db/db')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
+})
+
+afterEach(() => env.cleanup(testDb))
+
+describe('Taks database tests', () => {
+  it('getTasks returns an array of 3 tasks', () => {
+    expect.assertions(1)
+
+    const expected = 3
+
+    return db.getTasks(testDb)
+      .then(tasks => {
+        const actual = tasks.length
+        expect(actual).toBe(expected)
+      })
+    })
+})
