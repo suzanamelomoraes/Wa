c8766d1cb094fac34b5885730e896df7be618bbd
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-12-06 14:07:55 +1300

    fix tables moving image from tasks to users

--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { id: 1, name: 'Don', email: 'don@devacademy.com', mobile: 99999999, password: 'lacucaracha', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321 },
-        { id: 2, name: 'Bryce', email: 'bryce@devacademy.com', mobile: 99999999, password: 'potatoes', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321 },
-        { id: 3, name: 'Emily', email: 'emily@devacademy.com', mobile: 99999999, password: 'cool', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321 },
-        { id: 4, name: 'Jess', email: 'jess@devacademy.com', mobile: 99999999, password: 'great', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321 }
+        { id: 1, name: 'Don', email: 'don@devacademy.com', mobile: 99999999, password: 'lacucaracha', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'avatar01' },
+        { id: 2, name: 'Bryce', email: 'bryce@devacademy.com', mobile: 99999999, password: 'potatoes', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'avatar01' },
+        { id: 3, name: 'Emily', email: 'emily@devacademy.com', mobile: 99999999, password: 'cool', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'avatar01' },
+        { id: 4, name: 'Jess', email: 'jess@devacademy.com', mobile: 99999999, password: 'great', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'avatar01' }
       ])
     })
 }
