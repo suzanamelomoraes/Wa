ed7565f0423529ab615262edc5f0bc5822423378
samuel-looi <samhylooi@gmail.com>
2019-12-06 15:47:12 +1300

    CompleteMahi modal template done

--- client/components/CompleteMahi.jsx
+++ client/components/CompleteMahi.jsx
@@ -1,42 +1,60 @@
 import React, { Component } from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Form, Icon } from 'semantic-ui-react'
+// import { connect } from 'react-redux'
 
 class AcceptMahi extends Component {
-  render () {
-    return (
+state = {
+  showModal: false
+}
 
-      <div data-title="testingtestingtesting" style={{ margin: '5rem' }}>
-        <Modal trigger={<Button>Confirm Completion</Button>} centered={true}>
-          <Modal.Header style={{ fontSize: '2rem' }}>Confirm</Modal.Header>
-          <Modal.Content image>
-            {/* <Image wrapped size='medium' src='/images/avatar01.png' /> */}
-            <Modal.Description>
-              {/* <Header style={{ fontSize: '2rem' }}>Teach me to swim</Header> */}
-              <div style={{ fontSize: '1.25rem' }}>
-                <p>
-									Are you sure your task is completed?
-                </p>
-              </div>
-            </Modal.Description>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button
-              negative
-              icon='frown outline'
-              labelPosition='right'
-              content="No, I am not"
-            />
-            <Button
-              positive
-              icon='smile outline'
-              labelPosition='right'
-              content="Yes, I am"
-            />
-          </Modal.Actions>
-        </Modal>
-      </div>
-    )
-  }
+handleCreateButton = (event) => {
+  event.preventDefault()
+  this.setState({
+    showModal: false
+  })
+}
+
+render () {
+  return (
+
+    <div data-title="testingtestingtesting" style={{ margin: '5rem' }}>
+      <Modal open={this.state.showModal} trigger={<Button onClick={() => this.setState({ showModal: true })}>Completed</Button>} centered={true}>
+        <Modal.Header style={{ fontSize: '2rem' }}>Confirm</Modal.Header>
+        <Modal.Content image>
+          {/* <Image wrapped size='medium' src='/images/avatar01.png' /> */}
+          <Modal.Description>
+            {/* <Header style={{ fontSize: '2rem' }}>Teach me to swim</Header> */}
+            <div style={{ fontSize: '1.25rem' }}>
+              <p>
+Are you sure your task is completed?
+              </p>
+            </div>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            negative
+            icon='frown outline'
+            labelPosition='right'
+            content="No, I am not"
+            size="large"
+            onClick={(evt) => this.handleCreateButton(evt)}
+          />
+          <Button
+            positive
+            icon='smile outline'
+            labelPosition='right'
+            content="Yes, I am"
+            size="large"
+            onClick={(evt) => this.handleCreateButton(evt)}
+          />
+        </Modal.Actions>
+      </Modal>
+    </div>
+  )
+}
 }
 
 export default AcceptMahi
+
+// connect()()
