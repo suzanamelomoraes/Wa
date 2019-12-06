eaeaf0b1dbbacf34f877f0a991450bf1b451df9b
Jeya Raniga <jayafj@gmail.com>
2019-12-06 21:30:51 +1300

    completed error component tests

--- tests/client/components/Error.test.js
+++ tests/client/components/Error.test.js
@@ -5,11 +5,19 @@ import { Message } from 'semantic-ui-react'
 import { Error } from '../../../client/components/Error'
 
 describe('Error component tests', () => {
-  it('renders Message component if error', () => {
+  it('renders Message if error is not null', () => {
     const error = 'an error'
     const component = mount(<Error error={error}/>)
 
     const actual = component.containsMatchingElement(Message)
     expect(actual).toBeTruthy()
   })
+  it('does not render Message if error is null', () => {
+    const error = null
+    const component = mount(<Error error={error}/>)
+
+    const actual = component.containsMatchingElement(Message)
+    expect(actual).toBeFalsy()
+  })
+
 })
