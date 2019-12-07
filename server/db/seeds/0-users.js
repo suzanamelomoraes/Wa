04b2d6770c2595c224337745422d472638ad0209
Andria-Hibe <andriacohibe@gmail.com>
2019-12-07 15:43:16 +1300

    Change seed and db function to send location

--- server/db/seeds/0-users.js
+++ server/db/seeds/0-users.js
@@ -4,10 +4,10 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { id: 1, name: 'Don', email: 'don@devacademy.com', mobile: 99999999, password: 'lacucaracha', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'images/avatar01.png' },
-        { id: 2, name: 'Bryce', email: 'bryce@devacademy.com', mobile: 99999999, password: 'potatoes', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'images/avatar01.png' },
-        { id: 3, name: 'Emily', email: 'emily@devacademy.com', mobile: 99999999, password: 'cool', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'images/avatar01.png' },
-        { id: 4, name: 'Jess', email: 'jess@devacademy.com', mobile: 99999999, password: 'great', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: 1234, longitude: 4321, image: 'images/avatar01.png' }
+        { id: 1, name: 'Don', email: 'don@devacademy.com', mobile: 99999999, password: 'lacucaracha', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: -36.864571, longitude: 174.776006, image: 'images/avatar01.png' },
+        { id: 2, name: 'Bryce', email: 'bryce@devacademy.com', mobile: 99999999, password: 'potatoes', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: -36.861601, longitude: 174.774053, image: 'images/avatar01.png' },
+        { id: 3, name: 'Emily', email: 'emily@devacademy.com', mobile: 99999999, password: 'cool', balance: 3, address: '12 Morgan Street, Newmarket, 0600', latitude: -36.865753, longitude: 174.779259, image: 'images/avatar01.png' },
+        { id: 4, name: 'Jess', email: 'jess@devacademy.com', mobile: 99999999, password: 'great', balance: 3, address: '97 Carlton Gore Road, Newmarket, Auckland 1023', latitude: -36.864380, longitude: 174.772875, image: 'images/avatar01.png' }
       ])
     })
 }
