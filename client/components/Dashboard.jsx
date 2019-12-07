b2fc99ee12cc95e3beef7e09789c09d4f084dd6d
Jeya Raniga <jayafj@gmail.com>
2019-12-07 16:10:13 +1300

    profile component with passing tests

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 
 import Profile from './Profile'
 
+// to be removed once there are props passed down
+const profileprops = {
+  name: 'Steve Jobs',
+  image: '/images/avatar02.png',
+  about: 'Coding Enthusiast',
+  mobile: '021 456 4445',
+  email: 'steve.jobs@apple.com',
+  address: '12 Morgan Street, Newmarket, Auckland, NewZealand'
+}
 export class Dashboard extends Component {
   state = { }
   render () {
     return (
-      <Profile />
+      <Profile user={profileprops}/>
     )
   }
 }
--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,27 +1,73 @@
 import React, { Component } from 'react'
-import { Card, Icon, Image, Button, Message } from 'semantic-ui-react'
+import { Card, Icon, Image, Table, Header, Segment } from 'semantic-ui-react'
 
-class Profile extends Component {
+export class Profile extends Component {
   state = { }
   render () {
+    const { name, image, about, mobile, email, address } = this.props.user
     return (
-      <Card>
-        <Card.Content>
-          <Image
-            floated='right'
-            size='mini'
-            src='/images/steve.jpg'
-          />
-          <Card.Header>Steve Jobs</Card.Header>
-          <Card.Meta>Language Enthusiast</Card.Meta>
-          <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-      
-        </Card.Content>
-      </Card>
+      <Segment compact style={{ marginTop: 75, size: 'small' }}>
+        <Card fluid>
+          <Card.Content>
+            <Image
+              floated='right'
+              size='small'
+              src={image}
+            />
+            <Card.Header>{name}</Card.Header>
+            <Card.Meta>{about}</Card.Meta>
+          </Card.Content>
+          <Card.Content extra textAlign='center'>
+          </Card.Content>
+        </Card>
+        <Table basic padded >
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Description</Table.HeaderCell>
+              <Table.HeaderCell>Your Details</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Icon name='phone' size='small'/>
+                  <Header.Content>
+          Mobile
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>{mobile}</Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Icon name='mail' size='small'/>
+                  <Header.Content>
+          Email
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>{email}</Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Icon name='address card' size='small'/>
+                  <Header.Content>
+          Address
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>{address}</Table.Cell>
+            </Table.Row>
+
+          </Table.Body>
+        </Table>
+      </Segment>
     )
   }
 }
