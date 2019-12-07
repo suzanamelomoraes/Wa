0ec3d6a1f0cf69cabca8e1d61c4a1fffe5168326
Jeya Raniga <jayafj@gmail.com>
2019-12-08 11:33:25 +1300

    added basic component

---
+++ client/components/OfferingList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class OfferingList extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <h1>OfferingComponent</h1>
+      </div>
+    )
+  }
+}
+ 
+export default OfferingList
