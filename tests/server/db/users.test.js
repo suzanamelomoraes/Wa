a808ba75da25e14ea4e5a53e04f2295a830cc302
Marina Tateoka <marinaslussarek@gmail.com>
2019-12-07 14:54:35 +1300

    create Users db test

---
+++ tests/server/db/users.test.js
@@ -0,0 +1,25 @@
+const env = require('./test.environment')
+const db = require('../../../server/db/db')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialise(testDb)
+})
+
+afterEach(() => env.cleanup(testDb))
+
+describe('Users database tests', () => {
+  it('getUsers returns an array of users object', () => {
+    expect.assertions(1)
+
+    const expected = 4
+
+    return db.getUsers(testDb)
+      .then(users => {
+        const actual = users.length
+        expect(actual).toBe(expected)
+      })
+  })
+})
