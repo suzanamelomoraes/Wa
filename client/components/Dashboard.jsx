9c42c8392b78897bcf00128a78b1671369bb75bd
Jeya Raniga <jayafj@gmail.com>
2019-12-07 18:56:47 +1300

    added redux tests not working

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 
 import Profile from './Profile'
+import TimeCurrency from './TimeCurrency'
+import AddMahi from './AddMahi'
 
 // to be removed once there are props passed down
-const profileprops = {
+const props = {
   name: 'Steve Jobs',
   image: '/images/avatar02.png',
   about: 'Coding Enthusiast',
   mobile: '021 456 4445',
   email: 'steve.jobs@apple.com',
-  address: '12 Morgan Street, Newmarket, Auckland, NewZealand'
+  address: '12 Morgan Street, Newmarket, Auckland, NewZealand',
+  balance: 8
 }
 export class Dashboard extends Component {
   state = { }
   render () {
     return (
-      <Profile user={profileprops}/>
+      <div>
+        <Profile user={props}/>
+        <TimeCurrency props={props}/>
+        <AddMahi/>
+
+      </div>
+
     )
   }
 }
