3d92237fc539710426825bcfdd8625a20ee23e54
samuel-looi <samhylooi@gmail.com>
2019-12-07 17:03:55 +1300

    remove test that simulates click which calls api

--- tests/client/components/CompleteMahi.test.js
+++ tests/client/components/CompleteMahi.test.js
@@ -53,25 +53,4 @@ describe('<CompleteMahi /> component test', () => {
     expect(state1).toBe(true)
     expect(state2).toBe(false)
   })
-  it("When CompleteMahi Component accept button clicked state.showModal becomes false", () => {
-    // Arrange
-    const component = mount(<CompleteMahi/>)
-    const firstButton = component.find('button[data-test="firstBtn"]')
-    const expectedRes = { 'id': 1, 'categoryId': 2, 'assignerId': 3, 'title': 'walk with dogs', 'description': 'walk with dogs', 'status': 'completed', 'hours': 1, 'assignee': 2 }
-    nock(apiURL)
-      .put('/completed')
-      .reply(200, expectedRes)
-
-    // Act
-    firstButton.simulate('click')
-    const state1 = component.instance().state.showModal
-    const modalButtons = component.find('button[data-test="secondBtnPositive"]')
-    modalButtons.simulate('click')
-
-    const state2 = component.instance().state.showModal
-
-    // Assert
-    expect(state1).toBe(true)
-    expect(state2).toBe(false)
-  })
 })
