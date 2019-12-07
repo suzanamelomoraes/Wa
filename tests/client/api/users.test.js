d61a5ceeddac22081c2ae5c15e0bcc8a0905c935
Jeya Raniga <jayafj@gmail.com>
2019-12-07 17:33:37 +1300

    added id

--- tests/client/api/users.test.js
+++ tests/client/api/users.test.js
@@ -4,20 +4,22 @@ const apiURL = 'http://localhost:3000'
 const { getUserDetails } = require('../../../client/api/users')
 
 describe('Testing tasks api', () => {
-  it('getTasks function returns expected array', () => {
+  it('getUserDetails function returns expected object', () => {
     const expected = {
+      id: 1,
       name: 'Steve Jobs',
       image: '/images/avatar02.png',
       about: 'Coding Enthusiast',
       mobile: '021 456 4445',
       email: 'steve.jobs@apple.com',
-      address: '12 Morgan Street, Newmarket, Auckland, NewZealand'
+      address: '12 Morgan Street, Newmarket, Auckland, New Zealand'
     }
+    const id = 1
     nock(apiURL)
-      .get('/api/v1/users')
+      .get('/api/v1/users/1')
       .reply(200, expected)
 
-    return getUserDetails
+    return getUserDetails(id)
       .then(userDetails => {
         expect(userDetails).toEqual(expected)
       })
