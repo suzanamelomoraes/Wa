4d66ab8d1b5b287176760b3131342a466c127201
samuel-looi <samhylooi@gmail.com>
2019-12-07 16:18:00 +1300

    create & change tests, change client-side completeTask api route

--- tests/client/components/CompleteMahi.test.js
+++ tests/client/components/CompleteMahi.test.js
@@ -2,6 +2,8 @@ import CompleteMahi from '../../../client/components/CompleteMahi'
 import { mount, shallow } from 'enzyme'
 import React from 'react'
 import { Button, Header, Image, Modal, Form, Icon } from 'semantic-ui-react'
+const nock = require('nock')
+const apiURL = 'http://locahost:3000/api/v1/tasks/'
 
 test('truthy test', () => {
   expect(true).toBeTruthy()
@@ -34,7 +36,7 @@ describe('<CompleteMahi /> component test', () => {
     expect(beforeShowModal).toBe(false)
     expect(afterShowModal).toBeTruthy()
   })
-  it("When popup's buttons are clicked state.showModal becomes false", () => {
+  it("When CompletTask Component cancel button clicked state.showModal is false", () => {
     // Arrange
     const component = mount(<CompleteMahi/>)
     const firstButton = component.find('button[data-test="firstBtn"]')
@@ -42,7 +44,28 @@ describe('<CompleteMahi /> component test', () => {
     // Act
     firstButton.simulate('click')
     const state1 = component.instance().state.showModal
-    const modalButtons = component.find('button[data-test="secondBtn"]')
+    const modalButtons = component.find('button[data-test="secondBtnNegative"]')
+    modalButtons.simulate('click')
+
+    const state2 = component.instance().state.showModal
+
+    // Assert
+    expect(state1).toBe(true)
+    expect(state2).toBe(false)
+  })
+  it("When CompleteMahi Component accept button clicked state.showModal becomes false", () => {
+    // Arrange
+    const component = mount(<CompleteMahi/>)
+    const firstButton = component.find('button[data-test="firstBtn"]')
+    const expectedRes = { 'id': 1, 'categoryId': 2, 'assignerId': 3, 'title': 'walk with dogs', 'description': 'walk with dogs', 'status': 'completed', 'hours': 1, 'assignee': 2 }
+    nock(apiURL)
+      .put('/completed')
+      .reply(200, expectedRes)
+
+    // Act
+    firstButton.simulate('click')
+    const state1 = component.instance().state.showModal
+    const modalButtons = component.find('button[data-test="secondBtnPositive"]')
     modalButtons.simulate('click')
 
     const state2 = component.instance().state.showModal
