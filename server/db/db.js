7f43b4eaa5f3f3cc43bcf14fa776b8ac10886a6f
Suzana de Melo de Moraes <suzanamelojor@gmail.com>
2019-12-07 18:09:40 +1300

    run seeds to fix a bug

--- server/db/db.js
+++ server/db/db.js
@@ -11,6 +11,13 @@ function getTasks (db = connection) {
     .select('tasks.id as taskId', 'tasks.cat_id as categoryId', 'tasks.assigner as assignerId', 'tasks.name as title', 'categories.name as category', 'tasks.description as description', 'status', 'tasks.time as hours', 'users.image as image', 'users.name as assignerName')
 }
 
+function getTask (id, db = connection) {
+  return db('tasks')
+    .where('id', id)
+    .select('id', 'cat_id as categoryId', 'assigner as assignerId', 'name as title', 'description', 'status', 'time as hours', 'assignee as assignee')
+    .first()
+}
+
 function selectTask (id, assignee, db = connection) {
   return db('tasks')
     .where('id', id)
@@ -30,12 +37,6 @@ function completeTask (id,db = connection) {
     .then(() => getTask(id, db))
 }
 
-function getTask (id, db = connection) {
-  return db('tasks')
-    .where('id', id)
-    .select('id', 'cat_id as categoryId', 'assigner as assignerId', 'name as title', 'description', 'status', 'time as hours', 'assignee as assignee')
-    .first()
-}
 
 function addTask (categoryId, { assignerId, title, description, status, hours }, db = connection) {
   return db('tasks').insert({
