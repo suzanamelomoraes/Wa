df665c3ad5215e3f481e0564487aed7907982268
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2019-12-09 11:17:07 +1300

    create NotificationAddMahi component

---
+++ client/components/NotificationAddMahi.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Message, Container } from 'semantic-ui-react'
+
+export class NotificationAddMahi extends Component {
+render ()
+
+}
+
+export default connect(mapStateToPropos) (NotificationAddMahi)
