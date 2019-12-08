acfbbc3eb26b6b88d2effdfffbc391026bb97532
Jeya Raniga <jayafj@gmail.com>
2019-12-08 14:05:04 +1300

    added offering reducers with test

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -4,10 +4,12 @@ import error from './error'
 import categories from './categories'
 import pending from './pending'
 import tasks from './tasks'
+import offerings from './offerings'
 
 export default combineReducers({
   error,
   categories,
   pending,
-  tasks
+  tasks,
+  offerings
 })
--- client/reducers/pending.js
+++ client/reducers/pending.js
@@ -4,14 +4,21 @@ import {
 } from '../actions/categories'
 
 import {
+  GET_OFFERINGS_SUCCESS,
+  GET_OFFERINGS_PENDING
+} from '../actions/tasks'
+
+import {
   SET_ERROR
 } from '../actions/error'
 
 export default function (pending = false, action) {
   switch (action.type) {
     case GET_CATEGORIES_PENDING:
+    case GET_OFFERINGS_PENDING:
       return true
     case GET_CATEGORIES_SUCCESS:
+    case GET_OFFERINGS_SUCCESS:
     case SET_ERROR:
       return false
     default:
