d68a139870a3d98b8ca4ddd49b249a0d845db782
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2019-12-09 12:20:51 +1300

    create notification to display when mahi is added to dashboard

--- client/components/NotificationAddMahi.jsx
+++ client/components/NotificationAddMahi.jsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Message, Container } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Message, Container } from "semantic-ui-react";
 
 export class NotificationAddMahi extends Component {
-render ()
-
+  render() {
+    return (
+      <Container>
+        {this.props.notification && (
+          <Message negative floating>
+            <Message.Header>
+              Mahi has been added to your dashboard
+            </Message.Header>
+            <p>{this.props.notification}</p>
+          </Message>
+        )}
+      </Container>
+    );
+  }
 }
 
-export default connect(mapStateToPropos) (NotificationAddMahi)
+const mapStateToProps = state => {
+  return {
+    notification: state.notification
+  };
+};
+
+export default connect(mapStateToProps)(NotificationAddMahi);
