650d544c27be7c38ecb4adac3aeec8354ce18909
te-piha <ttrwwn@hotmail.com>
2019-12-08 14:33:01 +1300

    build api getVolunteer

---
+++ client/api/users.js
@@ -0,0 +1,10 @@
+import request from 'superagent'
+
+const apiURL = 'http://localhost:3000/api/v1/users'
+
+export function getVolunteer (id) {
+  return request
+    .get(`${apiURL}/${id}`)
+    .then(res => res.body)
+    .catch(err => { throw new Error(err.message) })
+}
