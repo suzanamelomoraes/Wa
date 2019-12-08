cf30a4f1c7a4d4e671ec3e2fcb293a46ca613d43
Samuel Hoe Yan Looi <samhylooi@gmail.com>
2019-12-08 15:35:41 +1300

    Mocked out some data

--- client/components/OfferingMahi.jsx
+++ client/components/OfferingMahi.jsx
@@ -5,7 +5,13 @@ import CompleteMahi from './CompleteMahi'
 import SelectOfferingMahi from './SelectOfferingMahi'
 
 export default class OfferingMahi extends Component {
+
   render () {
+    this.props = testInfo
+    console.log(this.state.props)
+    console.log(this.props)
+    console.log(this.props)
+    console.log(this.props)
     return (
       <>
         <Card>
@@ -28,4 +34,17 @@ export default class OfferingMahi extends Component {
       </>
     )
   }
+}
+
+var testInfo = {
+  title: 'Find my tracker',
+  status: 'In Progress',
+  assignee: 'Jekkins Jones',
+  assigner: 'Bob Farmer',
+  assigneeId: 2,
+  assignerId: 1,
+  category: 'Outdoor and garden',
+  hours: 5,
+  taskId: 2,
+  description: "Pls find mer tractors. I can't find it and without it I can't till the fields!"
 }
