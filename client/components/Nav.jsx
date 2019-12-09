e9799ad4baf263fca499226d9b50dfed1d4c8302
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-12-09 16:14:45 +1300

    change the fontsize

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,22 +4,22 @@ import { Container, Header, Menu } from 'semantic-ui-react'
 
 const NavBar = () => {
   return (
-    <>
-      <Menu borderless fixed='top' inverted style = {{ backgroundColor: '#123b26' }}>
-        <Container>
-          <Menu.Item as={Link} to='/' header >
-            <Header data-test="header" as='h1' inverted style={{ fontSize: '2.5rem' }}>Wā</Header>
-          </Menu.Item>
-          <Menu.Item style={{ fontSize: '1.5rem' }} as={Link} to='/'>Home</Menu.Item>
-          <Menu.Item style={{ fontSize: '1.5rem' }} as={Link} to='/list'>Mahi</Menu.Item>
-          <Menu.Item style={{ fontSize: '1.5rem' }} as={Link} to='/dashboard'>Dashboard</Menu.Item>
-          <Menu.Item position='right'>
-            <Menu.Item style={{ fontSize: '1.5rem' }} as={Link} to='./register'>Register</Menu.Item>
-            <Menu.Item style={{ fontSize: '1.5rem' }} as={Link} to='/signin'>Sign In</Menu.Item>
-          </Menu.Item>
-        </Container>
-      </Menu>
-    </>
+<>
+<Menu borderless fixed='top' inverted style = {{ backgroundColor: '#123b26' }}>
+  <Container>
+    <Menu.Item as={Link} to='/' header >
+      <Header data-test="header" as='h1' inverted >Wā</Header>
+    </Menu.Item>
+    <Menu.Item as={Link} to='/'>Home</Menu.Item>
+    <Menu.Item as={Link} to='/list'>Mahi</Menu.Item>
+    <Menu.Item as={Link} to='/dashboard'>Dashboard</Menu.Item>
+    <Menu.Item position='right'>
+      <Menu.Item as={Link} to='./register'>Register</Menu.Item>
+      <Menu.Item as={Link} to='/signin'>Sign In</Menu.Item>
+    </Menu.Item>
+  </Container>
+</Menu>
+</>
   )
 }
 
