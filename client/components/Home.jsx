ecd9ebe167f4d4945370ec25918a65e52f137696
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-12-09 20:52:13 +1300

    add proper text for headers

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -14,7 +14,46 @@ const inlineText = <div >
     <Header.Content>
       With your food basket  <br/>
       and my food basket <br/>
-      the people will thrive test
+      the people will thrive
+    </Header.Content>
+  </Header>
+</div>
+
+const mahiText = <div>
+  <Header
+    textAlign='center'
+    style={{
+      fontSize: '30px',
+      textShadow: '1px 1px #000000',
+      color: '#ffffff' }}>
+    <Header.Content>
+taking and offering mahi
+    </Header.Content>
+  </Header>
+</div>
+
+const comunnityText = <div>
+  <Header
+    textAlign='center'
+    style={{
+      fontSize: '30px',
+      textShadow: '1px 1px #000000',
+      color: '#ffffff' }}>
+    <Header.Content>
+empowering communities
+    </Header.Content>
+  </Header>
+</div>
+
+const exchangeText = <div>
+  <Header
+    textAlign='center'
+    style={{
+      fontSize: '30px',
+      textShadow: '1px 1px #000000',
+      color: '#ffffff' }}>
+    <Header.Content>
+Using Wa/Time as a currency
     </Header.Content>
   </Header>
 </div>
@@ -28,11 +67,7 @@ const Home = () => {
   }
   return (
   <>
-  <Container
-    style={{ width: '100%',
-      marginTop: 60
-    }}
-  >
+  <Container style={{ width: '100%', marginTop: 60 }}>
     <Image
       bordered
       style={{
@@ -42,45 +77,43 @@ const Home = () => {
         margin: 0,
         padding: 0,
         opacity: 0.5,
-        // backgroundImage: `url(${'images/placeholder.jpg'})`,
-        // backgroundImage: `url(${'images/tomatoes.jpg'})`,
-        // backgroundImage: `url(${'images/hands.jpg'})`,
-        // backgroundImage: `url(${'images/handHearts.jpg'})`,
         backgroundImage: `url(${'images/waitangi.jpg'})`,
         backgroundSize: 'cover',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexWrap: 'wrap'
-      }}
-    >
+      }}>
       {inlineText}
     </Image>
   </Container>
 
   <Divider inverted />
-
-  <Header style={centerStyle} size='huge'> Getting started </Header>
-
+  <Header style={centerStyle} size='huge'> Getting Started </Header>
   <Divider inverted />
-
     <Grid colums='equal' style={centerStyle}>
-
       <Grid.Column mobile={12} tablet={8} computer={4}>
         <Header as='h3' attached='top' textAlign='center' size='huge' style={{ backgroundColor: '#123b26', color: 'white' }} >
         Mahi
         </Header>
-        <Container style={{ width: '100%' }}>
-          <Image src='images/placeholder.jpg' fluid
-            style={{ opacity: 0.5 }}
-          />
-
+        <Container attached>
+          <Image
+            style={{ opacity: 0.5,
+              width: '100%',
+              height: '250px',
+              backgroundImage: `url(${'images/placeholder.jpg'})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap' }}>
+            {mahiText}
+          </Image>
         </Container>
-
         <Segment attached size='huge' textAlign='center'>
-          <br></br>
           <p>
-            in literal sense mahi is the act of working/perfomring, Wa uses Mahi as a concept of offering a task doing what the people need and helping the commuity hauiahauih
+            In literal sense mahi is the act of working or doing, Wa uses Mahi as a concept of offering a task. we need 5 lines here bruh.  we need 5 lines here bruh.  we need 5 lines here bruh.  
           </p>
         </Segment>
       </Grid.Column>
@@ -90,21 +123,28 @@ const Home = () => {
         Community
         </Header>
 
-        <Container style={{ width: '100%',
-          height: '100%'
-        }}>
-          <Image src='images/hands.jpg' fluid
-            style={{ opacity: 0.5 }}
-          />
-          {/* Create a mahi to share with your community and select a mahi to help out.
-          </Image> */}
+        <Container attached >
+          <Image
+            style={{ opacity: 0.5,
+              width: '100%',
+              height: '250px',
+              backgroundImage: `url(${'images/hands.jpg'})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap' }}
+          >
+
+            {comunnityText}
+          </Image>
 
         </Container>
 
         <Segment attached size='huge' textAlign='center'>
           <p>
-            a is very communtiy based platform as it aims to build a strong connection with those around you, become more aware of the skills you have and what others have to offer
-            Contribute to the community by helping complete mahi
+            communtiy based platform that aims to build a strong connection with those around you, become more aware of the skills you have and what others have to offer
           </p>
         </Segment>
       </Grid.Column>
@@ -114,25 +154,29 @@ const Home = () => {
         Exchange
         </Header>
 
-        <Container style={{ width: '100%',
-          height: '100%'
-        }}>
-          <Image src='images/tomatoes.jpg' fluid
-            style={{ opacity: 0.5 }}
-          />
-          {/* Create a mahi to share with your community and select a mahi to help out.
-          </Image> */}
+        <Container attached>
+          <Image
+            style={{ opacity: 0.5,
+              width: '100%',
+              height: '250px',
+              backgroundImage: `url(${'images/tomatoes.jpg'})`,
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexWrap: 'wrap' }}
+          >
+
+            {exchangeText}
+          </Image>
 
         </Container>
 
         <Segment attached size='huge' textAlign='center'>
-          <p>      
+          <p>
             using your time to do the things that really matter
-
-            exchaning time to do what you want to and doing the things that you cant do
-
-            by offering an alternative to apprecaite and empower the communities
-
+            time to do what you want to and doing the things that you cant do
             Once mahi has been completed you exchange Wa/time with
           </p>
         </Segment>
