c374a8a084ece9ecc1b484eecdff828a88ca7bea
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2019-12-09 14:25:35 +1300

    update client to display notification

--- client/api/tasks.js
+++ client/api/tasks.js
@@ -1,34 +1,44 @@
-import request from 'superagent'
+import request from "superagent";
 
-const apiURL = 'http://localhost:3000/api/v1/tasks'
+const apiURL = "http://localhost:3000/api/v1/tasks";
 
-const err = 'An unexpected error has occurred and we are looking into it'
+const err = "An unexpected error has occurred and we are looking into it";
 
-export function getTasks () {
-  return request.get(apiURL)
+export function getTasks() {
+  return request
+    .get(apiURL)
     .then(res => res.body)
-    .catch(err => { throw new Error(err.message) })
+    .catch(err => {
+      throw new Error(err.message);
+    });
 }
 
-export function completeTask (id, assignerId, assigneeId, time) {
-  return request.put(apiURL + `/completed`)
+export function completeTask(id, assignerId, assigneeId, time) {
+  return request
+    .put(apiURL + `/completed`)
     .send({ id, assignerId, assigneeId, time })
     .then(res => res.body)
-    .catch(err => { throw new Error(err.message) })
+    .catch(err => {
+      throw new Error(err.message);
+    });
 }
 
-export function addMahi (mahi) {
-  return request.post(apiURL + '/newtask')
+export function addMahi(mahi) {
+  return request
+    .post(apiURL + "/newtask")
     .send(mahi)
     .then(res => res.body)
-    .catch(err => { throw new Error(err.message) })
+    .catch(err => {
+      throw new Error(err.message);
+    });
 }
 
-export function selectTask (id, assignee) {
+export function selectTask(id, assignee) {
   return request
     .put(apiURL)
     .send({ id, assignee })
     .then(res => res.body)
-    .catch(err => { throw new Error(err.message) })
+    .catch(err => {
+      throw new Error(err.message);
+    });
 }
-
