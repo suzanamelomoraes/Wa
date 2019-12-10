55b8f584fe4420d1ad622c77c6cf737fadeb68dd
Marina Tateoka <marinaslussarek@gmail.com>
2019-12-10 14:29:28 +1300

    Add balance on NavBar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Header, Menu } from 'semantic-ui-react'
 import { logOff } from 'authenticare/client'
+import { connect } from 'react-redux'
+import { getUser } from '../actions/user'
 
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 export class NavBar extends Component {
+  state = {
+    user: {}
+  }
 
+  componentDidMount () {
+    this.props.getUser()
+      .then(() =>
+        this.setState({
+          user: this.props.user
+        })
+      )
+  }
+  
   logOff = () => {
     this.props.history.push('/list')
     logOff()
   }
   render () {
+    const { user } = this.state
     return (
       <>
       <Menu borderless fixed='top' inverted style = {{ backgroundColor: '#123b26' }}>
@@ -30,6 +45,7 @@ export class NavBar extends Component {
               <Menu.Item as={Link} to='/signin'>Sign In</Menu.Item>
             </IfNotAuthenticated>
             <IfAuthenticated>
+              <Menu.Item> Balance <br/> {user.balance} Hours</Menu.Item>
               <Menu.Item as={Link} onClick={this.logOff} to='#'>Sign Out</Menu.Item>
             </IfAuthenticated>
           </Menu.Item>
@@ -41,4 +57,14 @@ export class NavBar extends Component {
   }
 }
 
-export default NavBar
+const mapDispatchToProps = {
+  getUser
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
