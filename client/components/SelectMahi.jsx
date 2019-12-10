cfe37560cd873fd7c627d5c8dcfc9179677d325e
Andria-Hibe <andriacohibe@gmail.com>
2019-12-11 11:55:33 +1300

    Make bold and underlined hours consistent

--- client/components/SelectMahi.jsx
+++ client/components/SelectMahi.jsx
@@ -35,7 +35,7 @@ export class SelectMahi extends Component {
             <p style={{ fontSize: '1.25em' }}>{details.description}</p>
 
             <p style={{ fontSize: '1.25em' }}>
-              You can earn {details.hours} hour(s) when you help out {details.assignerName}
+              You can earn <b><u>{details.hours} hour(s)</u></b> when you help out {details.assignerName}
             </p>
           </Modal.Description>
         </Modal.Content>
