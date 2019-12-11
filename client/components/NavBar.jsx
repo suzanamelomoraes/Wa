7664c591733fe5c7bd71b8bec7a46068e4bb32c2
te-piha <ttrwwn@hotmail.com>
2019-12-11 15:28:07 +1300

    merge

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -8,11 +8,10 @@ import { getUser } from '../actions/user'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
 export class NavBar extends Component {
-
   componentDidMount () {
     this.props.getUser()
   }
-  
+
   logOff = () => {
     this.props.history.push('/list')
     logOff()
@@ -22,30 +21,30 @@ export class NavBar extends Component {
     const { user } = this.props
     return (
       <>
-      
-      <Menu borderless fixed='top' inverted style = {{ backgroundColor: '#123b26', height: '80px'}} >
-      
-          <Menu.Item as={Link} to='/' header >
-            <Header data-test="header" as='h1' inverted style={{ fontSize: '2.5rem' }}>Wā</Header>
-          </Menu.Item>
-          <Menu.Item as={Link} to='/'>Home</Menu.Item>
-          <Menu.Item as={Link} to='/list'>Mahi</Menu.Item>
+
+      <Menu borderless fixed='top' inverted style = {{ backgroundColor: '#123b26', height: '80px' }} >
+
+        <Menu.Item as={Link} to='/' header >
+          <Header data-test="header" as='h1' inverted style={{ fontSize: '2.5rem' }}>Wā</Header>
+        </Menu.Item>
+        <Menu.Item as={Link} to='/'>Home</Menu.Item>
+        <Menu.Item as={Link} to='/list'>Mahi</Menu.Item>
+        <IfAuthenticated>
+          <Menu.Item as={Link} to='/dashboard'>Dashboard</Menu.Item>
+        </IfAuthenticated>
+
+        <Menu.Menu position='right' >
+          <IfNotAuthenticated>
+            <Menu.Item as={Link} to='./register'>Register</Menu.Item>
+            <Menu.Item as={Link} to='/signin'>Sign In</Menu.Item>
+          </IfNotAuthenticated>
+
           <IfAuthenticated>
-            <Menu.Item as={Link} to='/dashboard'>Dashboard</Menu.Item>
+            <Menu.Item> Balance <br/>{user.balance} Hours</Menu.Item>
+            <Image style = {{ height: '65px' }} src={user.image}/>
+            <Menu.Item as={Link} onClick={this.logOff} to='#'>Sign Out</Menu.Item>
           </IfAuthenticated>
-        
-          <Menu.Menu position='right' >
-            <IfNotAuthenticated>
-                <Menu.Item as={Link} to='./register'>Register</Menu.Item>
-                <Menu.Item as={Link} to='/signin'>Sign In</Menu.Item>
-            </IfNotAuthenticated>
-           
-            <IfAuthenticated>
-                <Menu.Item> Balance <br/>{user.balance} Hours</Menu.Item>
-                <Image style = {{ height: '65px' }} src={user.image}/>
-                <Menu.Item as={Link} onClick={this.logOff} to='#'>Sign Out</Menu.Item>
-            </IfAuthenticated>
-          </Menu.Menu>
+        </Menu.Menu>
       </Menu>
       </>
     )
